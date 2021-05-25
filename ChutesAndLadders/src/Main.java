9aacb5f886879576bc4ace78c004c054553fda91
annikahoag <annikajhoag@gmail.com>
2021-05-25 13:33:51 -0400

    1. Updated main to have import statements

--- ChutesAndLadders/src/Main.java
+++ ChutesAndLadders/src/Main.java
@@ -1,9 +1,11 @@
+  
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("hi");
-
-	}
-
+class Main{
+  public static void main(String args[]) {
+    
+  }
+  
 }
