8ea4ee09391328de6396523b775c7876cafeb333
AaditiP <52939094+AaditiP@users.noreply.github.com>
2021-05-28 11:10:21 -0400

    Creates board object

--- ChutesAndLadders/src/Main.java
+++ ChutesAndLadders/src/Main.java
@@ -1,10 +1,11 @@
- import javax.swing.*;
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 class Main{
   public static void main(String args[]) {
-    
+    Board board = new Board();
+    board.goesFirst();
   }
   
 }
