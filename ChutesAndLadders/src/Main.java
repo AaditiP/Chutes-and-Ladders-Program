321490fa3d717905d355730b26833c9e08d96e6b
AaditiP <52939094+AaditiP@users.noreply.github.com>
2021-05-28 12:53:40 -0400

    Runs additional methods

--- ChutesAndLadders/src/Main.java
+++ ChutesAndLadders/src/Main.java
@@ -1,12 +1,10 @@
 import javax.swing.*;
-import java.util.Arrays;
 import java.util.Scanner;
-
 class Main{
-  public static void main(String args[]) {
-    Board board = new Board();
-    board.goesFirst();
-    
-  }
-  
+    public static void main(String args[]){
+        Board board = new Board();
+        board.goesFirst();
+        board.origBoard();
+        board.spaceSet();
+    }
 }
